2e46d389ddcdff69e5d46cdbe16117ad34c29210
FienvdH <fienvdhof@gmail.com>
2020-06-09 20:43:27 +0100

    Ganzenbord zonder put

---
+++ GanzenbordZonderPut.java
@@ -0,0 +1,18 @@
+package com.mycompany.gooseboard;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author fien_
+ */
+public class Ganzenbord {
+
+    public static void main(String[] args) {
+        
+        Spel spel1 = new Spel();
+
+        
+    }
+}
